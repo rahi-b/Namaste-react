722fec5d4c91678780d24c1ce4153a26ed67d32f
Abdul rahib <rahibrahib6740@gmail.com>
2024-11-02 11:38:24 +0530

    laying of foundation

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-const parent=React.createElement('div',{id:'parent'},
-    [
-        React.createElement('div',{id:'child1'},[
-            React.createElement('h1',{},"I'm an h1 tag"),
-            React.createElement('h2',{},"I'm an h2 tag")
-       
-        ]),
-        React.createElement('div',{id:'child2'},[
-            React.createElement('h1',{},"I'm an h1 tag"),
-            React.createElement('h2',{},"I'm an h2 tag")
-       
-        ]),
-    ]
-  
-        )
+import React from "react";
+import ReactDOM from 'react-dom/client';
 
-        console.log(parent);
+const heading=
+React.createElement('h1',{id:"heading"},"Namaste react Koii");
+console.log(heading)
 
-const Root=ReactDOM.createRoot(document.getElementById('root'));
+const jsxElement=<h1 id="heading">Namaste react use in jsx</h1>;
 
-Root.render(parent);
+console.log(jsxElement)
+const root=ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(jsxElement);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 #Namaste react
+
+#parcel
+
+-Dev build
+-Local server
+-HMR=Hot module replacement
+-File watching algorithum - written by c++
+-catching - Faster building
+-Image optimization
+-Minification
+-Bundling
+-Compress
+-Consistent hashing
+-Code splitting
+-Differential bundling-Support older browser
+-Diagnostic 
+-Error handling
+-HTTPs
+-Tree shaking - remove unusedcode
+-Different dev and prod bundles
